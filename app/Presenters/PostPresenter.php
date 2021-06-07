79317fbe68287209cd8f6ca38071a1262c581e5d
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-06-07 22:25:13 +0200

    create & edit article

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -59,6 +59,59 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 
 		$this->flashMessage('Děkuji za komentář', 'success');
 		$this->redirect('this');
-	
+	}
+
+	protected function createComponentPostForm(): Form
+	{
+		$form = new Form;
+		$form->addText('title', 'Titulek')
+			->setRequired();
+		$form->addTextArea('content', 'Obsah:')
+			->setRequired();
+
+		$form->addSubmit('send', 'Uložit a publikovat');
+		$form->onSuccess[] = [$this, 'postformSucceeded'];
+
+		return $form;
+	}
+
+	public function postFormSucceeded(Form $form, array $values): void
+	{
+		$postId = $this->getParameter('postId');
+
+		if ($this->getUser()->isLoggedIn()) {
+			$this->error('Pro vytvoření, nebo editování příspěvku se musíte přihlásit.');
+		}
+
+		if ($postId) {
+			$post = $this->database->table('posts')->get($postId);
+			$post->update($values);
+		} else {
+			$post = $this->database->table('posts')->insert($values);
+		}
+
+		$this->flashMessage("Příspěvek byl úspěšně publikován.", 'success');
+		$this->redirect('show', $post->id);
+	}
+
+	public function actionEdit(int $postId): void
+	{
+		if (!$this->getUser()->isLoggedIn()) {
+			$this->redirect('Sign:in');
+		}
+		
+		
+		$post = $this->database->table('posts')->get($postId);
+		if (!$post) {
+			$this->error('Příspěvek nebyl nalezen');
+		}
+		$this['postForm']->setDefaults($post->toArray());
+	}
+
+	public function actionCreate(): void
+	{
+		if (!$this->getUser()->isLoggedIn()) {
+			$this->redirect('Sign:in');
+		}
 	}
 }
