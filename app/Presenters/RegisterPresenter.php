39c54190e20e1a434ac07c1df89541c823a39260
Patrik Mrnka <patrikmrnka@Patrik-MacBook-Pro.local>
2021-06-15 01:28:11 +0200

    new RegisterForm

---
+++ app/Presenters/RegisterPresenter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+class RegisterPresenter extends Nette\Application\UI\Presenter 
+{
+    private Nette\Database\Explorer $database;
+
+    public function __construct(Nette\Database\Explorer $database)
+    {
+        $this->database = $database; 
+    }
+
+    protected function createComponentRegisterForm(): Form
+    {
+        $form = new Form;
+
+        $form->addText('username', 'Uživatelské jméno: ')
+            ->setRequired("Prosím vyplňte své uživatelské jméno.");
+        $form->addEmail('email', 'E-mailová adresa: ')
+            ->setRequired("Prosím vyplňte svoji e-mailovou adresu.");
+        $form->addPassword('password', 'Heslo: ')
+            ->setRequired("Prosím vyplňte své heslo.");
+        $form->addPassword('passwordAgain', 'Heslo znovu: ')
+            ->setRequired("Prosím vyplňte své heslo znovu pro ověření.");
+        $form->addSubmit('send', 'Vytvořit účet');
+
+        $form->onSuccess[] = [$this, 'registerFormSucceeded'];
+        return $form;
+    }
+
+    public function registerFormSucceeded(\stdClass $values): void
+    {
+        $users = $this->database->table('users');
+
+        if (count($users->where('username', $values->username)) != 0) {
+            $this->flashMessage("Uživatelské jméno je již používáno", "wrong");
+            $this->redirect("this");
+        } else if (count($users->where('email', $values->email)) != 0) {
+            $this->flashMessage("E-mailová adresa už je používána", "wrong");
+            $this->redirect("this");
+        } else if ($values->password != $values->passwordAgain) {
+            $this->flashmessage("Hesla se neshodují", "wrong");
+            $this->redirect("this");
+        } else {
+            $users->insert([
+                'username' => $values->username,
+                'email' => $values->email,
+                'password' => $values->password,
+                'role' => "Member",
+            ]);
+            $this->flashMessage("Úspěšně jste se zaregistrovali.", "success");
+            $this->redirect('Homepage:');
+        }
+    }
+}
